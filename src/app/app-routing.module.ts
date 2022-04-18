40d28f57486c102ec67467d5979088d8c3a25074
Dien Minh Nguyen <diennm.hn@gmail.com>
2022-04-18 21:57:02 +0700

    install bootstrap, build component layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MessagesComponent } from './Components/messages/messages.component';
+import { ContactsComponent } from './Components/contacts/contacts.component';
+import { PreferencesComponent } from './Components/preferences/preferences.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'messages',
+    pathMatch: 'full',
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+  {
+    path: 'preferences',
+    component: PreferencesComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmailBoxComponent } from './Components/email-box/email-box.component';
+import { EmailPreviewComponent } from './Components/email-preview/email-preview.component';
+import { EmailDetailComponent } from './Components/email-detail/email-detail.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { MessagesComponent } from './Components/messages/messages.component';
+import { ContactsComponent } from './Components/contacts/contacts.component';
+import { PreferencesComponent } from './Components/preferences/preferences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailBoxComponent,
+    EmailPreviewComponent,
+    EmailDetailComponent,
+    NavBarComponent,
+    PageNotFoundComponent,
+    MessagesComponent,
+    ContactsComponent,
+    PreferencesComponent
   ],
   imports: [
     BrowserModule,
